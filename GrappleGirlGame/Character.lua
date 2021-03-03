794c05714e2302e08d032736ca597305e59fd519
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2021-03-03 15:12:02 -0500

    added a jump to the player

--- GrappleGirlGame/Character.lua
+++ GrappleGirlGame/Character.lua
@@ -1,4 +1,4 @@
-
+CHARACTER_CATAGORY=2
 
 Character = {}
 
@@ -9,9 +9,20 @@ function Character:new(o, world, pos, speed)
     self.speed = {x = speed.x or speed[1] or 150, y = speed.y or speed[2] or 150}
     self.mv = {u = false, d = false, r = false, l = false}
     self.shape = love.physics.newRectangleShape(20,20)
+
     self.body = love.physics.newBody( world, pos.x or pos[1], pos.y or pos[2], "dynamic")
+    self.body:setFixedRotation(true)
+
     self.fixture = love.physics.newFixture(self.body, self.shape, 1)
     self.fixture:setFriction(1)
+    self.fixture:setCategory(CHARACTER_CATAGORY)
+    self.fixture:setUserData(o)
+    -- setmetatable(self.fixture:getUserData().val, self)
+    -- self.fixture:getUserData().val = o
+
+
+    self.canJump = true;
+
     return o
 end
 
@@ -29,6 +40,10 @@ function Character:update(dt)
         self.body:applyForce(f, 0)
     end
 
+    if(self.canJump and love.keyboard.isDown("space"))then
+        self.canJump = false
+        self.body:applyLinearImpulse(0, -50)
+    end
 
 end
 
--- GrappleGirlGame/main.lua
+++ GrappleGirlGame/main.lua
@@ -1,6 +1,6 @@
 require("Character")
 require("love")
-
+FLOOR_CATAGORY = 1
 
 function love.load()
     baseWorld = love.physics.newWorld(0, 1000, false)
@@ -13,9 +13,26 @@ function love.load()
     floor.body = love.physics.newBody( baseWorld, love.graphics.getWidth()/2, love.graphics.getHeight(), "static")
     floor.fixture = love.physics.newFixture(floor.body, floor.shape, 1)
     floor.fixture:setFriction(1);
+    floor.fixture:setCategory(FLOOR_CATAGORY);
 end
 
 function love.update(dt)
+    contacts = baseWorld:getContacts( )
+    for i = 0,#baseWorld:getContacts( ) do
+        if contacts[i] ~= nil then
+            f1, f2 = contacts[i]:getFixtures()
+            if f1:getCategory() == FLOOR_CATAGORY and f2:getCategory() == CHARACTER_CATAGORY or f2:getCategory() == FLOOR_CATAGORY and f1:getCategory() == CHARACTER_CATAGORY then
+                if f1:getCategory() == FLOOR_CATAGORY then
+                    charFic=f2
+                else
+                    charFic=f1
+                end
+                charFic:getUserData().canJump = true
+            end
+            -- print()
+        end
+    end
+
     baseWorld:update(dt)
     gGirl:update(dt)
 end
